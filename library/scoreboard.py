0af0649ba66e24a70aa54ba40f9c8cb39ad10e05
thais <thaiscamachoo@gmail.com>
2023-04-04 22:40:28 -0300

    pipeline

--- library/scoreboard.py
+++ library/scoreboard.py
@@ -1,12 +1,21 @@
 import numpy as np
+from enum import Enum
+
+class Step(Enum):
+    ISSUE = 0,
+    READ = 1,
+    EXECUTE = 2,
+    WRITE = 3,
+    DONE = 4
 
 def gen_instructions_board(configuration):
     instructions = []
     for i in configuration:
         status = np.zeros(4, dtype=np.uint32)
         instructions.append({
-            'instruction': i,
+            'info': i,
             'status': status,
+            'step': Step.ISSUE
         })
     return instructions
 
@@ -28,12 +37,46 @@ class Scoreboard:
     def __init__(self, instructions, functional_units):
         functional_units = gen_functional_units(functional_units)
         instructions = gen_instructions_board(instructions)
-        status = {
-            'register_f': np.zeros(10, dtype=np.uint32),
-            'register_i': np.zeros(10, dtype=np.uint32),
+        self.status = {
+            'register_f': np.zeros(20, dtype=np.uint32),
+            'register_i': np.zeros(20, dtype=np.uint32),
             'functional_units': functional_units,
             'instructions': instructions
         }
 
     def run(self):
-        print('running')
+        instruction_size = len(self.status['instructions'])
+        instruction_issue = 0
+        instruction = None
+        cycle = 1
+
+        while(instruction_issue < instruction_size):
+            for i in range(instruction_issue, -1, -1):
+                instruction = self.status['instructions'][i]
+
+                info = instruction['info']
+                status = instruction['status']
+                step = instruction['step']
+
+                if(step == Step.ISSUE):
+                    instruction['step'] = Step.READ
+                    status[0] = cycle
+                    instruction_issue = instruction_issue + 1
+                elif(step == Step.READ):
+                    instruction['step'] = Step.EXECUTE
+                elif(step == Step.EXECUTE):
+                    instruction['step'] = Step.WRITE
+                elif(step == Step.WRITE):
+                    instruction['step'] = Step.DONE
+                
+            cycle = cycle + 1
+
+    def dump_board(self):
+        instructions = self.status['instructions']
+        
+        print('ISSUE   -   READ   -   EXECUTE   -   WRITE   ')
+
+        for i in instructions:
+            status = i['status']
+            print(status)
+
