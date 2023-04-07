2a7805bb6bb1efcd5febe97b96ec95ea0673b3d0
thais <thaiscamachoo@gmail.com>
2023-04-07 14:29:40 -0300

    examples

---
+++ inputs/example4.s
@@ -0,0 +1,7 @@
+fdiv f1, f2, f3
+fdiv f1, f2, f3
+fdiv f1, f2, f3
+fdiv f1, f2, f3
+fdiv f1, f2, f3
+fdiv f1, f2, f3
+fdiv f1, f2, f3
---
+++ inputs/example5.s
@@ -0,0 +1,7 @@
+fdiv f1, f2, f3
+fdiv f2, f2, f3
+fdiv f3, f2, f3
+fdiv f4, f2, f3
+fdiv f5, f2, f3
+fdiv f6, f2, f3
+fdiv f7, f2, f3
