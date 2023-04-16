17d37a8a539ae11bf1c73953327329baa184fd9a
thais <thaiscamachoo@gmail.com>
2023-04-16 00:00:53 -0300

    up

---
+++ inputs/example10.s
@@ -0,0 +1,6 @@
+fld  f6, 34(x2)
+fld  f2, 45(x3)
+fmul f0, f2, f4
+fsub f8, f6, f2
+fdiv f10, f0, f6
+fadd f6, f8, f2
---
+++ inputs/example11.s
@@ -0,0 +1,6 @@
+fld f1, 0(x1)
+fld f5, 0(x1)
+fdiv f2, f4, f5
+fmul f4, f8, f9
+fadd f1, f2, f3 
+fsd f4, 0(x2)
